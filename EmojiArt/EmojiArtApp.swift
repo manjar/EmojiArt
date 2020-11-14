8b6ba06836eb4a592581f776055f4d9650661f61
Eli Manjarrez <manjarrez@apple.com>
2020-11-13 17:40:23 -0800

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Eli Manjarrez on 11/13/20.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
